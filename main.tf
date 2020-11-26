9717f0386f01f2165fa40ad78c8b13dfd3dbaa38
jaynro <jaynro@gmail.com>
2020-11-25 23:48:10 -0600

    Update main.tf
    Change the VMSS for a scalable cluster

--- main.tf
+++ main.tf
@@ -2,156 +2,179 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "test" {
   name     = "${var.prefix}-resources"
   location = var.location
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
+resource "azurerm_virtual_network" "test" {
+  name                = "test-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  tags = {
+    "Business unit" : "Test"
+  }
+
+
 }
 
-resource "azurerm_subnet" "internal" {
+resource "azurerm_subnet" "test" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_availability_set" "example" {
-  name                = "example-aset"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  tags = {
-    environment = "Test"
-  }
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "PublicIPForLB"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+resource "azurerm_public_ip" "test" {
+  name                = "test-pip"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Static"
-
   tags = {
-    environment = "Test"
+    "Business unit" : "Test"
   }
+
 }
 
-resource "azurerm_lb" "example" {
-  name                = "Test"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+
+
+
+#load balancer, which will need a backend address pool and address pool association for the network interface and the load balancer.
+
+resource "azurerm_lb" "test" {
+  name                = "test-lb"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
 
   frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example.id
+    name                 = "primary"
+    public_ip_address_id = azurerm_public_ip.test.id
   }
+
+  tags = {
+    "Business unit" : "Test"
+  }
+
+
 }
 
-resource "azurerm_lb_backend_address_pool" "bpepool" {
-  resource_group_name = azurerm_resource_group.main.name
-  loadbalancer_id     = azurerm_lb.example.id
-  name                = "BackEndAddressPool"
+
+
+resource "azurerm_lb_backend_address_pool" "test" {
+  resource_group_name = azurerm_resource_group.test.name
+  loadbalancer_id     = azurerm_lb.test.id
+  name                = "acctestpool"
+}
+
+# NIC references the address pool association
+resource "azurerm_network_interface" "test" {
+  name                = "test-nic"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+ tags = {
+    "Business unit" : "Test"
+  }
+
+
+
+}
+
+# Backend address pool association references NIC and backend address pool
+resource "azurerm_network_interface_backend_address_pool_association" "test" {
+  network_interface_id    = azurerm_network_interface.test.id
+  ip_configuration_name   = "testconfiguration1"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.test.id
 }
 
 
+resource "azurerm_managed_disk" "test" {
+ count                = var.capacity
+ name                 = "datadisk_existing_${count.index}"
+ location             = azurerm_resource_group.test.location
+ resource_group_name  = azurerm_resource_group.test.name
+ storage_account_type = "Standard_LRS"
+ create_option        = "Empty"
+ disk_size_gb         = "1023"
 
+ tags = {
+    "Business unit" : "Test"
+  }
+
+
+}
+
+resource "azurerm_availability_set" "avset" {
+ name                         = "avset"
+ location                     = azurerm_resource_group.test.location
+ resource_group_name          = azurerm_resource_group.test.name
+ platform_fault_domain_count  = 2
+ platform_update_domain_count = 2
+ managed                      = true
+    tags = {
+    "Business unit" : "Test"
+  }
+
+}
+
+
+# image
 data "azurerm_resource_group" "image" {
-  name = "UDACITY-RG"
+  name = "udacity-images"
 }
+
 
 data "azurerm_image" "image" {
   name                = "myPackerImage"
   resource_group_name = data.azurerm_resource_group.image.name
 }
 
-resource "azurerm_virtual_machine_scale_set" "vmss" {
-  name                = "vmscaleset"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.main.name
-  upgrade_policy_mode = "Manual"
 
-  sku {
-    name     = "Standard_B1s"
-    tier     = "Standard"
-    capacity = var.capacity
+# Decalre VM
+# Relates with NIC and Availabilty SET
+
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "test-machine"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.test.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
-  storage_profile_image_reference {
-    id=data.azurerm_image.image.id
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_profile_os_disk {
-    name              = ""
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
+        source_image_id=data.azurerm_image.image.id
 
-  storage_profile_data_disk {
-    lun          = 0
-    caching        = "ReadWrite"
-    create_option  = "Empty"
-    disk_size_gb   = 10
-  }
 
-  os_profile {
-    computer_name_prefix = "vmlab"
-    admin_username       = "azureuser"
-    admin_password       = "Passwword1234"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-
-    ssh_keys {
-      path     = "/home/azureuser/.ssh/authorized_keys"
-      key_data = file("~/.ssh/id_rsa.pub")
-
-    }
-  }
-
-  network_profile {
-    name    = "terraformnetworkprofile"
-    primary = true
-
-    ip_configuration {
-      name                                   = "IPConfiguration"
-      subnet_id                              = azurerm_subnet.internal.id
-      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
-      primary = true
-    }
-  }
-  
-  tags = {
-    environment = "test"
+    tags = {
+    environment = "staging",
+    "Business unit" : "Test"
   }
 }
 
 
-
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_security_group" "example" {
+resource "azurerm_network_security_group" "test" {
   name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
 
   security_rule {
     name                       = "test123"
@@ -166,7 +189,14 @@ resource "azurerm_network_security_group" "example" {
   }
 
   tags = {
-    environment = "Test"
+    environment = "staging"
+    "Business unit" : "Test"
   }
 }
+
+
+
+
+
+
 
